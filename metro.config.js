047351beb13dee2da9dd4fc012453472509ded7f
RamboFetBB <tudaolanchesteste@gmail.com>
2026-08-30 17:05:41 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'ts', 'tsx'];
+
 module.exports = config;
