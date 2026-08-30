b692dfce4621496f53393f9c9c67550b6ed08d52
RamboFetBB <tudaolanchesteste@gmail.com>
2026-08-30 14:08:40 -0300

    Update Babel presets to include jsxRuntime option

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          jsxRuntime: 'automatic',
+          flow: false
+        }
+      ]
+    ],
     plugins: [
       ['@babel/plugin-transform-flow-strip-types', { loose: true }]
     ]
